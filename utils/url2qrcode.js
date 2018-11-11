99302164556e034d64d0918532aebaa43df1da1d
bestvist <bestvist@qq.com>
2018-11-11 16:39:29 +0800

    modify: qrcode ora

--- utils/url2qrcode.js
+++ utils/url2qrcode.js
@@ -1,11 +1,14 @@
 const qt = require('qrcode-terminal');
 const chalk = require('chalk');
+const ora = require('ora');
 const log = console.log;
 
 const fn = function (url) {
+    const spinner = ora('Loading generate').start();
     qt.generate(url, {
         small: true
     }, function (qrcode) {
+        spinner.stop();
         log(chalk.hex('#4ac51d')(qrcode));
     });
 }
